e63fdc4660c27632ce782f0f3c0fd299a234855b
Crackvignoule <kiki.kalagan@gmail.com>
2024-05-25 21:19:14 +0200

    wip

--- .gitbook/assets/plex.sh
+++ .gitbook/assets/plex.sh
@@ -4,49 +4,51 @@ RED='\033[0;31m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
-echo "Welcome to the Plex setup script !"
-echo "This script will install the latest version of Plex Media Server and other services."
+update_and_install_dependencies() {
+    echo -e "${GREEN}\nUpdating system and installing dependencies (rclone, curl, whiptail)...\n${NC}"
+    sudo apt update
+    sudo apt full-upgrade -y
+    sudo apt install -y curl rclone whiptail
+    sudo apt autoremove
+    sudo apt clean
+}
 
-# Verify if docker and docker compose (not docker-compose) are installed, if not install them
-if ! command -v docker > /dev/null; then
-    # echo "Docker is not installed. Installing Docker ..."
-    # sudo usermod -aG docker $USER
-    # curl -fsSL https://get.docker.com | sh
-    echo "Docker is not installed. Please install Docker and run the script again."
-fi
-if ! command -v docker compose > /dev/null; then
-    echo "Docker Compose is not installed. Installing Docker Compose ..."
-    echo "Please install Docker Compose and run the script again."
-fi
+check_docker_installation() {
+    if ! command -v docker > /dev/null; then
+        # echo "Docker is not installed. Installing Docker ..."
+        # sudo usermod -aG docker $USER
+        # curl -fsSL https://get.docker.com | sh
+        echo "Docker is not installed. Please install Docker and run the script again."
+    fi
+    if ! command -v docker compose > /dev/null; then
+        echo "Docker Compose is not installed. Please install Docker Compose and run the script again."
+    fi
+}
 
-echo -e "${GREEN}\nUpdating system and installing dependencies (rclone, curl, whiptail)...\n${NC}"
-sudo apt update
-sudo apt full-upgrade -y
-sudo apt install -y curl rclone whiptail
-sudo apt autoremove
-sudo apt clean
+configure_and_mount_rclone() {
+    echo -e "${BLUE}\nPlease set up your remote for source files, when done just quit config with q\n${NC}"
+    rclone config
+    if rclone listremotes > /dev/null 2>&1; then
+        rclone mount my-remote:links /home/plex/data/rclone --dir-cache-time 10s --allow-other
+    else
+        echo "rclone configuration failed or no remotes configured. Exiting."
+        exit 1
+    fi
+}
 
-echo -e "${BLUE}\nPlease set up your remote for source files (WebDav ...)\n${NC}"
-rclone config
-
-# Check if rclone configuration was successful
-if rclone listremotes > /dev/null 2>&1; then
-
-    # Mount the remote
-    rclone mount my-remote:links /home/plex/data/rclone --dir-cache-time 10s --allow-other
-
+display_and_process_checklist() {
     # Define the checkable options
     CHECKABLE_OPTIONS=(
-        "Plex Debrid" "Description for Plex Debrid" OFF
-        "Jackett" "Description for Jackett" OFF
-        "Flaresolverr" "Description for Flaresolverr" OFF
-        "Overseerr" "Description for Overseerr" OFF
-        "Tautulli" "Description for Tautulli" OFF
+        "Plex Debrid" "Used to treat users requests" ON
+        "Jackett" "Scrapper" ON
+        "Flaresolverr" "May be used by Jackett to Bypass Captcha" ON
+        "Overseerr" "Provides cool Web UI for user requests" OFF
+        "Tautulli" "Management Dashboard for Plex" OFF
     )
 
     # Show the checklist dialog for checkable options
     CHOICES=$(whiptail --title "Select Options" --checklist \
-    "Choose options using space and navigate with arrow keys" 15 50 5 \
+    "Choose options using space and navigate with arrow keys" 15 65 5 \
     "${CHECKABLE_OPTIONS[@]}" 3>&1 1>&2 2>&3)
 
     # Check exit status
@@ -84,9 +86,10 @@ if rclone listremotes > /dev/null 2>&1; then
         esac
     done
 
-    # Continue with additional commands
     echo "You have selected the options: ${SELECTED_OPTIONS[*]}. Proceeding with the next steps."
+}
 
+construct_and_execute_docker_compose() {
     # Construct the docker-compose command based on the selected options
     DOCKER_COMPOSE_COMMAND="curl -L https://raw.githubusercontent.com/Crackvignoule/Wiki/main/.gitbook/assets/docker-compose.yml -o - | docker compose -f - up -d"
     for OPTION in "${SELECTED_OPTIONS[@]}"; do
@@ -98,8 +101,14 @@ if rclone listremotes > /dev/null 2>&1; then
 
     echo "Everything placed under /home/plex/"
     exit 0
+}
 
-else
-    echo "rclone configuration failed or no remotes configured. Exiting."
-    exit 1
-fi
+
+echo "Welcome to the Plex setup script !"
+echo "This script will install the latest version of Plex Media Server and other services."
+
+check_docker_installation
+update_and_install_dependencies
+configure_and_mount_rclone
+display_and_process_checklist
+construct_and_execute_docker_compose
